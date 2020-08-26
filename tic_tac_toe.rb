80b8a6dc06286343667bb916c7ca680cf021fed6
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2020-08-26 07:51:43 -0700

    Add available spaces instance variable containing array of available spaces / possible moves

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -34,6 +34,18 @@ class GenerateNewGame
       space_8,
       space_9,
     ]
+
+    @available_spaces = [
+      space_1,
+      space_2,
+      space_3,
+      space_4,
+      space_5,
+      space_6,
+      space_7,
+      space_8,
+      space_9,
+    ]
   end
 end
 
