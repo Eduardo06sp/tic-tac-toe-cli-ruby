22e446a3765e38e0079a3c69ed6b232e5dc8930e
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2020-08-27 01:21:08 -0700

    Add all methods to GenerateNewGame class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -63,127 +63,127 @@ class GenerateNewGame
     @player1_sign = p1_sign
     @player2_sign = p2_sign
   end
-end
 
-def validate_input(values, input)
-  values.any? do |value|
-    value == input
-  end
-end
-
-def start_new_game
-  puts "Would you like to play Tic-Tac-Toe?"
-  puts "Please enter y (yes) or n (no)."
-
-  answer = gets.chomp
-
-  if answer == "y"
-    puts "What would you like Player 1 to be named?"
-    player_1_name = gets.chomp
-
-    puts "What would you like Player 2 to be named?"
-    player_2_name = gets.chomp
-
-    puts '#{player_1_name}, please choose a sign: "X" or "O"'
-    player_1_sign = gets.chomp.upcase
-    if validate_input(["x", "O"], player_1_sign)
-      continue
-    else
-      puts 'Please input "X" or "O"'
-      player_1_sign = gets.chomp.upcase
+  def validate_input(values, input)
+    values.any? do |value|
+      value == input
     end
-
-    player_1_sign == "X" ? player_2_sign = "O" : player_2_sign = "X"
-    game = GenerateNewGame.new(player_1_name, player_2_name, player_1_sign, player_2_sign)
-  else
-    puts "Have a beautiful day. :}"
-    return
   end
-end
 
-def play
-  while !(game_over)
-    turn = p1
+  def start_new_game
+    puts "Would you like to play Tic-Tac-Toe?"
+    puts "Please enter y (yes) or n (no)."
 
-    if validate_input(available_spaces, move)
-      check_turn(turn)
+    answer = gets.chomp
+
+    if answer == "y"
+      puts "What would you like Player 1 to be named?"
+      player_1_name = gets.chomp
+
+      puts "What would you like Player 2 to be named?"
+      player_2_name = gets.chomp
+
+      puts '#{player_1_name}, please choose a sign: "X" or "O"'
+      player_1_sign = gets.chomp.upcase
+      if validate_input(["x", "O"], player_1_sign)
+        continue
+      else
+        puts 'Please input "X" or "O"'
+        player_1_sign = gets.chomp.upcase
+      end
+
+      player_1_sign == "X" ? player_2_sign = "O" : player_2_sign = "X"
+      game = GenerateNewGame.new(player_1_name, player_2_name, player_1_sign, player_2_sign)
     else
-      puts "That space is unavailable.
+      puts "Have a beautiful day. :}"
+      return
+    end
+  end
+
+  def play
+    while !(game_over)
+      turn = p1
+
+      if validate_input(available_spaces, move)
+        check_turn(turn)
+      else
+        puts "That space is unavailable.
       \nPlease type in one of the following available spaces: 
       #{available_spaces}"
 
-      check_turn(turn)
-    end
-  end
-end
-
-def turn(name, sign)
-  puts "#{name} (#{sign}) it's your turn."
-  puts "Please select an open spot."
-  move = gets.chomp
-
-
-  spaces[move - 1]=(sign)
-  available_spaces.delete(spaces[move - 1])
-
-  puts game_board
-end
-
-def check_turn(player_turn)
-  if turn == p1
-    turn(player_1_name, player_1_sign)
-    turn = p2
-  else
-    turn(player_2_name, player_2_sign)
-    turn = p1
-  end
-end
-
-def end_game(outcome)
-  puts "Game over!"
-
-  if outcome == player_1_sign
-    puts "#{player_1_name} won the match!"
-    @@player_1_score += 1
-  elsif outcome == player_2_sign
-    puts "#{player_2_name} won the match!"
-    @@player_2_score += 1
-  else
-    puts "Game ends in a tie."
-  end
-
-  start_new_game
-end
-
-def game_over
-  win_possibilities = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [7, 4, 1],
-    [8, 5, 2],
-    [9, 6, 3],
-    [3, 5, 7],
-    [1, 5, 9]
-  ]
-
-  if available_spaces.length = 0
-    end_game(nil)
-  end
-
-  win_possibilities.each do |win_array|
-    if (win_array.all? do |space|
-      spaces[space - 1] == "X"
-    end)
-      end_game("X")
+        check_turn(turn)
+      end
     end
   end
 
-  win_possibilities.each do |win_array|
-    if (win_array.all? do |space|
-      spaces[space - 1] == "O"
-    end)
-      end_game("O")
+  def turn(name, sign)
+    puts "#{name} (#{sign}) it's your turn."
+    puts "Please select an open spot."
+    move = gets.chomp
+
+
+    spaces[move - 1]=(sign)
+    available_spaces.delete(spaces[move - 1])
+
+    puts game_board
+  end
+
+  def check_turn(player_turn)
+    if turn == p1
+      turn(player_1_name, player_1_sign)
+      turn = p2
+    else
+      turn(player_2_name, player_2_sign)
+      turn = p1
+    end
+  end
+
+  def end_game(outcome)
+    puts "Game over!"
+
+    if outcome == player_1_sign
+      puts "#{player_1_name} won the match!"
+      @@player_1_score += 1
+    elsif outcome == player_2_sign
+      puts "#{player_2_name} won the match!"
+      @@player_2_score += 1
+    else
+      puts "Game ends in a tie."
+    end
+
+    start_new_game
+  end
+
+  def game_over
+    win_possibilities = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [7, 4, 1],
+      [8, 5, 2],
+      [9, 6, 3],
+      [3, 5, 7],
+      [1, 5, 9]
+    ]
+
+    if available_spaces.length = 0
+      end_game(nil)
+    end
+
+    win_possibilities.each do |win_array|
+      if (win_array.all? do |space|
+        spaces[space - 1] == "X"
+      end)
+        end_game("X")
+      end
+    end
+
+    win_possibilities.each do |win_array|
+      if (win_array.all? do |space|
+        spaces[space - 1] == "O"
+      end)
+        end_game("O")
+      end
     end
   end
 end
