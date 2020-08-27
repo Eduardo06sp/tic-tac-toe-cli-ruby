58b3e00a1e0110e3f9eafa89a0b13f829b092f11
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2020-08-27 00:31:40 -0700

    Create hash of arrays containing winning possibilities

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -137,4 +137,14 @@ def check_turn(player_turn)
 end
 
 def game_over
+  win_possibilities = {
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [7, 4, 1],
+    [8, 5, 2],
+    [9, 6, 3],
+    [3, 5, 7],
+    [1, 5, 9]
+  }
 end
