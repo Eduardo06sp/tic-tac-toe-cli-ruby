ea7a34dc832060515d32bafa35e07fe015f25059
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2020-08-28 01:19:21 -0700

    Simplify spaces instance variable, storing game board values directly in it

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -30,17 +30,7 @@ class GenerateNewGame
     @space_8 = "8"
     @space_9 = "9"
 
-    @spaces = [
-      space_1,
-      space_2,
-      space_3,
-      space_4,
-      space_5,
-      space_6,
-      space_7,
-      space_8,
-      space_9,
-    ]
+    @spaces = (1..9).to_a
 
     @available_spaces = [
       space_1,
